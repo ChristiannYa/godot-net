517e50d5484df6e242af29ff6f8e71cfb2b83c8b
ChristiannYa <christian.ylopez02@gmail.com>
2026-09-08 00:34:28 -0400

    feat(dropped stale packets)

--- globals/net/net_client.gd
+++ globals/net/net_client.gd
@@ -1,7 +1,11 @@
 extends Node
 
+const _BITS_LEN := 8
+
 var _peer := PacketPeerUDP.new()
+
 var _schema := GdPacketSchema.create()
+var _last_seq := {}
 
 func _ready():
 	_peer.bind(0)
@@ -13,6 +17,19 @@ func send_input(field_name: String, value: int):
 
 func _process(_delta: float):
 	while _peer.get_available_packet_count() > 0:
-		var buf: PackedByteArray = _peer.get_packet()
-		var fields: Dictionary = _schema.decode(buf)
+		var fields: Dictionary = _schema.decode(_peer.get_packet())
+		var sess_id: int = fields.get("SessionId")
+		var seq: int = fields.get("Seq")
+
+		if _last_seq.has(sess_id) and !_is_newer(seq, _last_seq[sess_id]):
+			# Dropped: stale/out-of-order packet
+			continue
+
+		_last_seq[sess_id] = seq
 		print("@Server: ", fields)
+
+func _is_newer(seq: int, last: int) -> bool:
+	var bits_max: int = 1 << _BITS_LEN
+	var diff: int = (seq - last + bits_max) % bits_max
+	return diff != 0 and diff < (bits_max >> 1)
+
