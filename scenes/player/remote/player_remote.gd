dcfbab6aabd8e16c68167a53489e2fbb792d0e03
ChristiannYa <christian.ylopez02@gmail.com>
2026-09-18 13:05:49 -0500

    refactor(remote player): old Net node's script contnet is in its main script file

--- scenes/player/remote/player_remote.gd
+++ scenes/player/remote/player_remote.gd
@@ -3,3 +3,9 @@ extends Player
 
 @onready var sid_label: Label3D = $SidLabel
 
+func _ready():
+	super._ready()
+	_set_sid_label.call_deferred()
+
+func _set_sid_label():
+	sid_label.text = "sid=%d" % sid
